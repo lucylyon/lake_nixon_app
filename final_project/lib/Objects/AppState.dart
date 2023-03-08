a529fc7edd9438acd0f9e2c15386b58aef3ac1a6
Lucy Lyon <lyonlg@hendrix.edu>
2023-03-07 23:20:19 -0600

    added getEvents & getSchedules(getSavedEvents) to appState
    they're not connected to things yet though

--- final_project/lib/Objects/AppState.dart
+++ final_project/lib/Objects/AppState.dart
@@ -21,9 +21,13 @@ class AppState extends ChangeNotifier {
   Color nixongreen = const Color.fromRGBO(81, 146, 78, 1);
 
   List<Event> _events = [];
-  List<Event> get events8 => _events;
+  List<Event> get events => _events;
 
-  // List<Event> events2 = [event1, event2, event3, event4, event5];
+  List<Appointment> _schedules = [];
+  List<Appointment> get schedules => _schedules;
+
+  Map<Group, List<Appointment>> _groupAppts = {};
+  Map<Group, List<Appointment>> get groupAppts => _groupAppts;
 
   AppState() {
     init();
@@ -31,33 +35,30 @@ class AppState extends ChangeNotifier {
 
   bool firstSnapshot = true;
   StreamSubscription<QuerySnapshot>? eventSubscription;
+  StreamSubscription<QuerySnapshot>? scheduleSubscription;
 
   Future<void> init() async {
     await Firebase.initializeApp(
         options: DefaultFirebaseOptions.currentPlatform);
 
-    // FirebaseUIAuth.configureProviders([
-    //   EmailAuthProvider(),
-    // ]);
-
     FirebaseAuth.instance.userChanges().listen(
       (user) {
-        eventSubscription?.cancel(); //WHY?
+        eventSubscription?.cancel();
+        scheduleSubscription?.cancel();
         print("starting to listen");
-        eventSubscription = FirebaseFirestore.instance
-            .collection('eventsListed') //GV had .doc(user.uid); document ref
-            .snapshots()
-            .listen((snapshot) {
-          print("in snapshot");
-          // _events = [];
-          // snapshot.docs.forEach((document) {
-          //   _events.add(Event(
-          //     title: document.data()['title'],
-          //     desc: document.data()['desc'],
-          //     time: document.data()['time'],
-          //     date: document.data()['date'],
-          //   ));
-        });
+        getEvents();
+        // eventSubscription = FirebaseFirestore.instance
+        //     .collection('events')
+        //     .snapshots()
+        //     .listen((snapshot) {
+        //   print("in snapshot");
+        //   snapshot.docs.forEach((document) {
+        //     _events.add(Event(
+        //         ageMin: document.data()['ageMin'],
+        //         groupMax: document.data()['groupMax'],
+        //         name: document.data()['name']));
+        //   });
+        // });
         // print(snapshot.docChanges.toString()); //prints changes
         firstSnapshot = false;
         notifyListeners();
@@ -82,26 +83,20 @@ class AppState extends ChangeNotifier {
     return -1;
   }
 
-  // from userCalendar / calendar
-  Future<void> getSavedEvents() async {
-    CollectionReference schedules =
-        FirebaseFirestore.instance.collection("schedules");
-    final snapshot = await schedules.get();
-    if (snapshot.size > 0) {
-      List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
-      data.forEach((element) {
-        var event = element.data() as Map;
+  Future<void> getSchedules() async {
+    scheduleSubscription = FirebaseFirestore.instance
+        .collection('schedules')
+        .snapshots()
+        .listen((snapshot) {
+      print("in schedule snapshot");
+      snapshot.docs.forEach((document) {
+        var event = document.data();
         Map apps = event["appointments"];
-
         apps.forEach((key, value) {
           for (var _app in value) {
             var app = _app["appointment"];
-            var test = app[2];
-            String valueString = test.split('(0x')[1].split(')')[0];
-            int value = int.parse(valueString, radix: 16);
-            Color color = new Color(value);
-            print(app[6]);
-            Appointment tmp = Appointment(
+            Color color = Color(value);
+            var tmp = Appointment(
                 startTime: app[0].toDate(),
                 endTime: app[1].toDate(),
                 color: color,
@@ -113,51 +108,219 @@ class AppState extends ChangeNotifier {
                 resourceIds: app[8],
                 recurrenceRule: app[9]);
             var group = indexGroups(key);
-            events[group]!.add(tmp);
+            groupAppts[group]!.add(tmp);
           }
         });
+        // var schedule = document.data() as Map;
+        // Map appts = schedule["appointments"];
+        // appts.forEach((key, value) {
+        //   for (var _appts in value){
+
+        //   }
+        // });
+        ///
+        ///
+
+        //         startTime: app[0].toDate(),
+        //         endTime: app[1].toDate(),
+        //         color: color,
+        //         startTimeZone: app[3],
+        //         endTimeZone: app[4],
+        //         notes: app[5],
+        //         isAllDay: app[6],
+        //         subject: app[7],
+        //         resourceIds: app[8],
+        //         recurrenceRule: app[9]);
+        //     var group = indexGroups(key);
+
+        // var event = document.data() as Map;
+        // Map apps = event["appointments"];
+        // apps.forEach((key, value) {
+        //   for (var _app in value) {
+        //     var app = _app["appointment"];
+        //     var test = app[2];
+        //     String valueString = test.split('(0x')[1].split(')')[0];
+        //     int value = int.parse(valueString, radix: 16);
+        //     Color color = new Color(value);
+        //     print(app[6]);
+        //     Appointment tmp = Appointment(
+        //         startTime: app[0].toDate(),
+        //         endTime: app[1].toDate(),
+        //         color: color,
+        //         startTimeZone: app[3],
+        //         endTimeZone: app[4],
+        //         notes: app[5],
+        //         isAllDay: app[6],
+        //         subject: app[7],
+        //         resourceIds: app[8],
+        //         recurrenceRule: app[9]);
+        //     var group = indexGroups(key);
+        //  events[group]!.add(tmp);
+        //events was Map<Group, Appointment>
+
+        // ));
       });
-    } else {
-      print('No data available.');
-    }
-  }
-
-// from userCalendar
-  List<DropdownMenuItem<String>> firebaseEvents = []; // goal delete this
-  Future<void> getEvents() async {
-    CollectionReference events =
-        FirebaseFirestore.instance.collection("events");
-    final snapshot = await events.get();
-    if (snapshot.size > 0) {
-      List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
-      data.forEach((element) {
-        var event = element.data() as Map;
-        var tmp = Event(
-            name: event["name"],
-            ageMin: event["ageMin"],
-            groupMax: event["groupMax"]);
-        events.add(tmp);
-
-        firebaseEvents.add(
-            DropdownMenuItem(value: event["name"], child: Text(event["name"])));
-      });
-    } else {
-      print('No data available.');
-    }
-    print(events);
-  }
-
-  Group? indexGroups(String name) {
-    int count = 0;
-    int index = -1;
-    Group? group;
-    events.forEach((key, value) {
-      if (key.name == name) {
-        index = count;
-        group = key;
-      }
-      count++;
     });
-    return group;
+  }
+
+  Future<void> getEvents() async {
+    eventSubscription = FirebaseFirestore.instance
+        .collection('events')
+        .snapshots()
+        .listen((snapshot) {
+      print("in event snapshot");
+      snapshot.docs.forEach((document) {
+        _events.add(Event(
+            ageMin: document.data()['ageMin'],
+            groupMax: document.data()['groupMax'],
+            name: document.data()['name']));
+      });
+    });
   }
 }
+
+Future<void> getSavedEvents() async {
+  CollectionReference schedules =
+      FirebaseFirestore.instance.collection("schedules");
+  final snapshot = await schedules.get();
+  if (snapshot.size > 0) {
+    List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
+    data.forEach((element) {
+      var event = element.data() as Map;
+      Map apps = event["appointments"];
+      apps.forEach((key, value) {
+        for (var _app in value) {
+          var app = _app["appointment"];
+          var test = app[2];
+          String valueString = test.split('(0x')[1].split(')')[0];
+          int value = int.parse(valueString, radix: 16);
+          Color color = new Color(value);
+          print(app[6]);
+          Appointment tmp = Appointment(
+              startTime: app[0].toDate(),
+              endTime: app[1].toDate(),
+              color: color,
+              startTimeZone: app[3],
+              endTimeZone: app[4],
+              notes: app[5],
+              isAllDay: app[6],
+              subject: app[7],
+              resourceIds: app[8],
+              recurrenceRule: app[9]);
+          var group = indexGroups(key);
+          events[group]!.add(tmp);
+          //events was Map<Group, Appointment>
+        }
+      });
+    });
+  } else {
+    print('No data available.');
+  }
+}
+
+Group? indexGroups(String name) {
+  int count = 0;
+  int index = -1;
+  Group? group;
+  events.forEach((key, value) {
+    if (key.name == name) {
+      index = count;
+      group = key;
+    }
+    count++;
+  });
+  return group;
+}
+
+//List<Event> dbEvents = [];
+
+// FIX
+// Future<void> getEvents() async {
+//   CollectionReference events =
+//       FirebaseFirestore.instance.collection("events");
+//   final snapshot = await events.get();
+//   if (snapshot.size > 0 && dbEvents.length == 0) {
+//     List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
+//     data.forEach((element) {
+//       var event = element.data() as Map;
+//       var tmp = Event(
+//           name: event["name"],
+//           ageMin: event["ageMin"],
+//           groupMax: event["groupMax"]);
+//       dbEvents.add(tmp);
+//     });
+//   } else {
+//     print('No data available.3');
+//   }
+//   for (Event event in dbEvents) {
+//     firebaseEvents
+//         .add(DropdownMenuItem(value: event.name, child: Text(event.name)));
+//   }
+//   print(dbEvents);
+// }
+
+// from userCalendar / calendar
+// Future<void> getSavedEvents() async {
+//   CollectionReference schedules =
+//       FirebaseFirestore.instance.collection("schedules");
+//   final snapshot = await schedules.get();
+//   if (snapshot.size > 0) {
+//     List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
+//     data.forEach((element) {
+//       var event = element.data() as Map;
+//       Map apps = event["appointments"];
+
+//       apps.forEach((key, value) {
+//         for (var _app in value) {
+//           var app = _app["appointment"];
+//           var test = app[2];
+//           String valueString = test.split('(0x')[1].split(')')[0];
+//           int value = int.parse(valueString, radix: 16);
+//           Color color = new Color(value);
+//           print(app[6]);
+//           Appointment tmp = Appointment(
+//               startTime: app[0].toDate(),
+//               endTime: app[1].toDate(),
+//               color: color,
+//               startTimeZone: app[3],
+//               endTimeZone: app[4],
+//               notes: app[5],
+//               isAllDay: app[6],
+//               subject: app[7],
+//               resourceIds: app[8],
+//               recurrenceRule: app[9]);
+//           var group = indexGroups(key);
+//           events[group]!.add(tmp);
+//           //events was Map<Group, Appointment>
+//         }
+//       });
+//     });
+//   } else {
+//     print('No data available.');
+//   }
+// }
+
+// from userCalendar
+// List<DropdownMenuItem<String>> firebaseEvents = []; // goal delete this
+// Future<void> getEvents() async {
+//   CollectionReference events =
+//       FirebaseFirestore.instance.collection("events");
+//   final snapshot = await events.get();
+//   if (snapshot.size > 0) {
+//     List<QueryDocumentSnapshot<Object?>> data = snapshot.docs;
+//     data.forEach((element) {
+//       var event = element.data() as Map;
+//       var tmp = Event(
+//           name: event["name"],
+//           ageMin: event["ageMin"],
+//           groupMax: event["groupMax"]);
+//       events.add(tmp);
+
+//       firebaseEvents.add(
+//           DropdownMenuItem(value: event["name"], child: Text(event["name"])));
+//     });
+//   } else {
+//     print('No data available.');
+//   }
+//   print(events);
+// }
